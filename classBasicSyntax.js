1eace319300e9e74c06b2ada1f2cb6ba86c02b38
vetacode <vetacode@gmail.com>
2025-11-26 13:42:55 +0700

    adding getters setters

--- classBasicSyntax.js
+++ classBasicSyntax.js
@@ -118,5 +118,30 @@ function makeClass(phrase) {
 
 // Create a new class
 let Guru = makeClass('Hello');
-
 new Guru().sayHi(); // Hello
+
+//GETTERS/SETTERS
+{
+  class User {
+    constructor(name) {
+      //invokes the setter
+      this.name = name;
+    }
+
+    get name() {
+      return this._name;
+    }
+
+    set name(value) {
+      if (value.length < 4) {
+        console.log('Name is too short.');
+        return;
+      }
+      this._name = value;
+    }
+  }
+
+  let user = new User('John');
+  console.log(user.name);
+  user = new User('');
+}
