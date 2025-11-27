adcd6eece01d3449458930565735c19f0fd21580
vetacode <vetacode@gmail.com>
2025-11-27 23:50:15 +0700

    finalizing mixins

--- mixins.js
+++ mixins.js
@@ -56,3 +56,109 @@ new User('Dude').sayHi(); // Hello Dude!
   // now User can say hi
   new User('Dude').sayHi(); // Hello Dude!
 }
+
+//EventMixin
+let eventMixin = {
+  /**
+   * Subscribe to event, usage:
+   *  menu.on('select', function(item) { ... }
+   */
+  on(eventName, handler) {
+    if (!this._eventHandlers) this._eventHandlers = {};
+    if (!this._eventHandlers[eventName]) {
+      this._eventHandlers[eventName] = [];
+    }
+    this._eventHandlers[eventName].push(handler);
+  },
+
+  /**
+   * Cancel the subscription, usage:
+   *  menu.off('select', handler)
+   */
+  off(eventName, handler) {
+    let handlers = this._eventHandlers?.[eventName];
+    if (!handlers) return;
+    for (let i = 0; i < handlers.length; i++) {
+      if (handlers[i] === handler) {
+        handlers.splice(i--, 1);
+      }
+    }
+  },
+
+  /**
+   * Generate an event with the given name and data
+   *  this.trigger('select', data1, data2);
+   */
+  trigger(eventName, ...args) {
+    if (!this._eventHandlers?.[eventName]) {
+      return; // no handlers for that event name
+    }
+
+    // call the handlers
+    this._eventHandlers[eventName].forEach((handler) =>
+      handler.apply(this, args)
+    );
+  },
+};
+
+//Usage
+// Make a class
+class Menu {
+  choose(value) {
+    this.trigger('select', value);
+  }
+}
+// Add the mixin with event-related methods
+Object.assign(Menu.prototype, eventMixin);
+
+let menu = new Menu();
+
+// add a handler, to be called on selection:
+menu.on('select', (value) => console.log(`Value selected: ${value}`));
+
+// triggers the event => the handler above runs and shows:
+// Value selected: 123
+menu.choose('123');
+
+{
+  let eventMixin = {
+    on(eventName, handler) {
+      if (!this._eventHandlers) this._eventHandlers = {};
+      if (!this._eventHandlers[eventName]) {
+        this._eventHandlers[eventName] = [];
+      }
+      this._eventHandlers[eventName].push(handler);
+    },
+
+    off(eventName, handler) {
+      let handlers = this._eventHandlers?.[eventName];
+      if (!handlers) return;
+      for (let i = 0; i < handlers.length; i++) {
+        if (handlers[i] === handler) {
+          handlers.splice(i--, 1);
+        }
+      }
+    },
+
+    trigger(eventName, ...args) {
+      if (!this._eventHandlers?.[eventName]) return;
+      this._eventHandlers[eventName].forEach((handler) =>
+        handler.apply(this, args)
+      );
+    },
+  };
+
+  class Menu {
+    choose(value) {
+      this.trigger('select', value);
+    }
+  }
+
+  Object.assign(Menu.prototype, eventMixin);
+
+  let menu = new Menu();
+
+  menu.on('select', (value) => console.log(`Value selected: ${value}`));
+
+  menu.choose('123'); // → logs correctly
+}
